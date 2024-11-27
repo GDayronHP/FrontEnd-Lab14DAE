a8d45e19881564347d7d1e00203ca7c9ba2cef0e
DiegoST001 <diegosancheztafur3@gmail.com>
2024-11-26 23:18:39 -0500

    ErroNavigation

--- front/src/pages/Dashboard.jsx
+++ front/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import Cuadro from "../components/AdminComponents/Cuadro";
 import Barra from "../components/AdminComponents/Barra";
 
